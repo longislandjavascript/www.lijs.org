bfef566242b9ca2fb56412ff7d0dd82fb5fafa36
gojutin <gojutin@gmail.com>
2022-05-21 16:22:09 -0400

    config: add Slack redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: [
+    { 
+      "source": "/slack",
+      "destination": "https://join.slack.com/t/longislandjavascript/shared_invite/zt-dy33kew4-bdy01_BOG8E7hh6NQTNaUA", 
+      "permanent": true
+    }
+  ]
 }
 
 module.exports = nextConfig
