c48d4df1026ab492f9ea4282d7131584862a46d4
just214 <21136155+just214@users.noreply.github.com>
2023-03-16 16:10:25 -0400

    feat: created reusable Airtable embd form

---
+++ src/components/AirtableEmbedForm.tsx
@@ -0,0 +1,30 @@
+import Script from "next/script";
+
+type Props = {
+  src: string;
+  height: string;
+  code?: string;
+};
+
+export function AirtableEmbedForm(props: Props) {
+  const src = props.code
+    ? `${props.src}?prefill_Code=${props.code}`
+    : props.src;
+  console.log(src);
+  return (
+    <>
+      <Script src="https://static.airtable.com/js/embed/embed_snippet_v1.js" />
+      <iframe
+        className="airtable-embed airtable-dynamic-height mt-8"
+        src={src}
+        width="100%"
+        height={props.height}
+        style={{
+          background: "transparent",
+          border: "1px solid lightgray",
+          borderRadius: "8px",
+        }}
+      />
+    </>
+  );
+}
