f0041c7542eccef5657e5c76bb27ffadd4ab5775
just214 <21136155+just214@users.noreply.github.com>
2023-03-16 16:10:06 -0400

    refactor: Updated layout component to accept className prop

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,11 +5,13 @@ import { Footer } from "components/Footer";
 type Props = {
   children?: React.ReactNode;
   pageTitle?: string;
+  className?: string;
 };
 
 export const Layout = (props: Props) => {
+  const { className = "", pageTitle, children } = props;
   return (
-    <div className="flex flex-col min-h-screen ">
+    <div className={`flex flex-col min-h-screen ${className}`}>
       <div className="hidden md:block">
         <Sidebar />
       </div>
@@ -19,9 +21,9 @@ export const Layout = (props: Props) => {
       <main
         className={`md:ml-80 py-4 px-4 md:px-12 mt-12 w-full container mx-auto max-w-4xl flex-1 pb-12`}
       >
-        {props.pageTitle && <h1 className="page-title">{props.pageTitle}</h1>}
+        {pageTitle && <h1 className="page-title">{pageTitle}</h1>}
 
-        {props.children}
+        {children}
       </main>
       <div className="md:hidden my-8 md:my-0">
         <Footer />
