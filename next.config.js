201a8acc5e8f39bcbb01ce51efce353cd200c3b9
just214 <21136155+just214@users.noreply.github.com>
2023-03-19 11:52:48 -0400

    fix: added CSP policy for improved security

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
     appDir: true,
   },
   reactStrictMode: true,
+  // https://beta.nextjs.org/docs/api-reference/components/image#dangerouslyallowsvg-and-contentsecuritypolicy
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
