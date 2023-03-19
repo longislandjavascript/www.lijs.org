c96e2aec82a25643046a53e33b04d2ec033331a0
just214 <21136155+just214@users.noreply.github.com>
2023-03-19 11:58:46 -0400

    feat: added improved content security header

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const ContentSecurityPolicy =
+  "script-src maps.googleapis.com;img-src data: maps.gstatic.com *.googleapis.com *.ggpht.com";
+
 module.exports = {
   experimental: {
     appDir: true,
@@ -7,5 +10,13 @@ module.exports = {
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  async headers() {
+    return [
+      {
+        key: "Content-Security-Policy",
+        value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+      },
+    ];
   },
 };
