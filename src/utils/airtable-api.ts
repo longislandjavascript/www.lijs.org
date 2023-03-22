54774065f58742b06ce2abd81ee79ca4614de6f5
just214 <21136155+just214@users.noreply.github.com>
2023-03-22 19:18:11 -0400

    fix: typo

--- src/utils/airtable-api.ts
+++ src/utils/airtable-api.ts
@@ -1,29 +1,29 @@
-
-import { NextApiRequest, NextApiResponse } from "next";
 import Airtable from "airtable";
+import { NextApiRequest, NextApiResponse } from "next";
 
 const base = new Airtable({ apiKey: process.env.AIRTABLE_TOKEN }).base(
   process.env.AIRTABLE_BASE_ID!
 );
 
 export function formSubmission(tableName: string) {
-  return (req: NextApiRequest, res: NextApiResponse ) => {
-  const body = req.body;
+  return (req: NextApiRequest, res: NextApiResponse) => {
+    const body = req.body;
 
-  base(tableName).create(
-    [
-      {
-        fields: body,
-      },
-    ],
-    function (err) {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ status: "error" });
+    base(tableName).create(
+      [
+        {
+          fields: body,
+        },
+      ],
+      function (err) {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ status: "error" });
+          return;
+        }
+        res.status(200).json({ status: "success" });
         return;
       }
-      res.status(200).json({ status: "success" });
-      return;
-    }
-  );
+    );
+  };
 }
