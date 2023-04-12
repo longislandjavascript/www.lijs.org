716c0c6094444e4e2c419a75bb631a80df1d175e
just214 <21136155+just214@users.noreply.github.com>
2023-04-12 19:46:03 -0400

    refactor: moved api routes to route handlers

---
+++ src/app/api/quiz/update-quiz-status/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+import { updateQuizStatusDetails } from "utils/airtable-api";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+  // const room_id = searchParams.get("room_id");
+  const admin_client_id = searchParams.get("admin_client_id");
+
+  try {
+    if (id === null || admin_client_id === null) {
+      return NextResponse.json({ success: false });
+    }
+    await updateQuizStatusDetails({
+      id,
+      admin_client_id,
+    });
+
+    return NextResponse.json({
+      success: true,
+      admin_client_id,
+    });
+  } catch (error) {
+    return NextResponse.json({ success: false });
+  }
+}
---
+++ src/app/api/quiz/validate-code/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server";
+
+import { findQuizByCode } from "utils/airtable-api";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const code = searchParams.get("code");
+  if (!code) {
+    return NextResponse.json({ success: false, error: "No code provided." });
+  }
+
+  try {
+    const results = await findQuizByCode(code);
+
+    if (!results.length) {
+      return NextResponse.json({
+        success: false,
+        error: "This is not a valid code.",
+      });
+    }
+    const { id, fields } = results[0];
+    const isAdmin = fields["Admin Code"] == +code;
+    if (fields["Status"] === "Ended") {
+      return NextResponse.json({
+        success: false,
+        error: "The quiz has ended.",
+      });
+    } else if (isAdmin) {
+      return NextResponse.json({
+        success: true,
+        record_id: id,
+        admin: isAdmin,
+      });
+    } else if (fields["Status"] !== "In Progress") {
+      return NextResponse.json({
+        success: false,
+        error: "This quiz is not active at this time.",
+      });
+    } else {
+      return NextResponse.json({
+        success: true,
+        record_id: id,
+      });
+    }
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      error: "Something went wrong. Please try again.",
+    });
+  }
+}
