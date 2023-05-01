b2868a46096f592c002b7075740e6aa95cdb2f67
just214 <21136155+just214@users.noreply.github.com>
2023-04-30 21:08:25 -0400

    feat: add photo page metadata

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 
+import { createMetadata } from "utils/createMetadata";
+
 import { Lightbox } from "./Lightbox";
+
+export const metadata = createMetadata({ title: "Photos", path: "/photos" });
 
 export default function App() {
   return <Lightbox />;
