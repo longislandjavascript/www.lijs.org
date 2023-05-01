189a100d8e16968905e1e5a8652ac6d1e5d08205
just214 <21136155+just214@users.noreply.github.com>
2023-04-30 20:51:42 -0400

    feat: added new nav links for photos and meetup review

--- src/constants/nav-links.ts
+++ src/constants/nav-links.ts
@@ -4,16 +4,18 @@ import {
   BsFillHouseHeartFill,
   BsGift,
   BsGithub,
+  BsImage,
   BsPatchQuestion,
   BsPeople,
   BsPiggyBank,
   BsSlack,
+  BsStarFill,
 } from "react-icons/bs";
 import { FaMeetup } from "react-icons/fa";
 import { GiScales } from "react-icons/gi";
 import { MdOutlineQuiz } from "react-icons/md";
 
-import { githubLink, meetupLink, slackLink } from "./links";
+import { githubLink, meetupLink, meetupReviewLink, slackLink } from "./links";
 
 export const navLinks = [
   { label: "Home", icon: BsFillHouseHeartFill, href: "/" },
@@ -21,6 +23,7 @@ export const navLinks = [
   { label: "Past Events", icon: BsClockHistory, href: "/past-events" },
   { label: "Code of Conduct", icon: GiScales, href: "/code-of-conduct" },
   { label: "FAQ", icon: BsPatchQuestion, href: "/faq" },
+  { label: "Photos", icon: BsImage, href: "/photos" },
   { label: "Quiz", icon: MdOutlineQuiz, href: "/quiz" },
   { label: "Redeem", icon: BsGift, href: "/redeem" },
   { label: "Donate", icon: BsPiggyBank, href: "/donate" },
@@ -28,4 +31,10 @@ export const navLinks = [
   { label: "Meetup", icon: FaMeetup, href: meetupLink, isExternal: true },
   { label: "Slack", icon: BsSlack, href: slackLink, isExternal: true },
   { label: "Github", icon: BsGithub, href: githubLink, isExternal: true },
+  {
+    label: "Leave a Review",
+    icon: BsStarFill,
+    href: meetupReviewLink,
+    isExternal: true,
+  },
 ];
