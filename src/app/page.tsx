e4ca54ac37945930ae59313fb1b8350f18f2799f
just214 <21136155+just214@users.noreply.github.com>
2023-06-23 15:12:08 -0500

    feat: updated homepage to address no upcoming event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { meetupLink } from "constants/links";
+import { showcaseItems } from "constants/showcase";
+
+import { ExternalLink } from "components/ExternalLink";
 import { GettingHere } from "components/GettingHere";
 import { MeetupEventDetails } from "components/MeetupEventDetails";
 import { Section } from "components/Section";
 import { ShowcaseCard } from "components/ShowcaseCard";
-import { showcaseItems } from "constants/showcase";
 import { createMetadata } from "utils/createMetadata";
 import { fetchNextEvent } from "utils/meetup-api";
 
@@ -15,11 +18,20 @@ export default async function IndexPage() {
   const nextEvent = await fetchNextEvent();
   return (
     <div className="w-full overflow-hidden">
-      {nextEvent && (
-        <Section title="Next Event">
-          <MeetupEventDetails event={nextEvent} type="future" />
-        </Section>
-      )}
+      <Section title="Next Event">
+        {nextEvent && <MeetupEventDetails event={nextEvent} type="future" />}
+        {!nextEvent && (
+          <div>
+            <p>
+              Please check back later or visit{" "}
+              <ExternalLink href={meetupLink} className="link">
+                our Meetup page
+              </ExternalLink>{" "}
+              for a list of all upcoming events.
+            </p>
+          </div>
+        )}
+      </Section>
 
       <div className="w-full mb-12 flex items-center gap-4 overflow-scroll snap-x snap-mandatory lg:snap-none lg:flex-wrap md:justify-around">
         {showcaseItems.map((item) => {
